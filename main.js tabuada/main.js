0feadbc7ff0b43c7565583420ef5d164e7c22f83
GustavoSobrera20 <gustavo.senac20@gmail.com>
2024-05-23 11:59:08 -0300

    Add files via upload

---
+++ main.js tabuada/main.js
@@ -0,0 +1,33 @@
+var x=1
+     
+  console.log ("tabuado do 1")
+    console.log( )
+
+console.log (x,"x 1 =",x*1)
+console.log (x,"x 2 =",x*2)
+console.log (x,"x 3 =",x*3)
+console.log (x,"x 4 =",x*4)
+console.log (x,"x 5 =",x*5)
+console.log (x,"x 6 =",x*6)
+console.log (x,"x 7 =",x*7)
+console.log (x,"x 8 =",x*8)
+console.log (x,"x 9 =",x*9)
+console.log (x,"x 10 =",x*10)
+
+console.log ()
+
+var x=87
+  console.log ("tabuado do 2")
+    console.log( )
+
+console.log (x,"x 1 =",x*1)
+console.log (x,"x 2 =",x*2)
+console.log (x,"x 3 =",x*3)
+console.log (x,"x 4 =",x*4)
+console.log (x,"x 5 =",x*5)
+console.log (x,"x 6 =",x*6)
+console.log (x,"x 7 =",x*7)
+console.log (x,"x 8 =",x*8)
+console.log (x,"x 9 =",x*9)
+console.log (x,"x 10 =",x*10)
+
